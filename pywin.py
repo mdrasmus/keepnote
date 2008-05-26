97754627b86b228946e0cdd32039a4d162b1efb7
unknown <rasmus@.(none)>
2008-05-26 15:07:38 -0400

    slimmed down gtk data files

--- pywin.py
+++ pywin.py
@@ -4,22 +4,25 @@
 
 """
 
-
-import win32api
+import os
 
 
 def get_local_drives():
-    """Get a list of all local drives"""
-    return [ drive for drive in
-             win32api.GetLogicalDriveStrings().split('\x00')[:-1]
-             if win32file.GetDriveType(drive) in (win32file.DRIVE_FIXED,
-                                                  win32file.DRIVE_CDROM,
-                                                  win32file.DRIVE_REMOVABLE)]
+    # hack that doesn't require win32api
+    for drive in "CDEFGHIJKLMNOPQRSTUVWXYZ":
+        if os.path.exists(drive + ":\\"):
+            yield drive + ":\\"
 
-def find_path(path, drives=get_local_drives()):
+
+def find_path(path, drives=None):
     """Find a path that exists on a list of drives"""
+	
+    if drives is None:
+        drives = get_local_drives()
+	
     for drive in drives:
         path2 = os.path.join(drive, path)
         if os.path.exists(path2):
             return path2
-    return None
+    
+    raise Exception("Path '%s' not found" % path)
