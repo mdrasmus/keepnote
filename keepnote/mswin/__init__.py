cac469738d303ee740830062e41a4d57dead312d
Matt Rasmussen <rasmus@mit.edu>
2011-10-18 09:39:48 -0400

    applied windows patch for finding My Documents

--- keepnote/mswin/__init__.py
+++ keepnote/mswin/__init__.py
@@ -1,9 +1,10 @@
 
 
-import sys
+
 
 # make sure py2exe finds win32com
 try:
+    import sys
     import modulefinder
     import win32com
     for p in win32com.__path__[1:]:
@@ -19,7 +20,9 @@ except ImportError:
 
 
 try:
-    from win32com.shell import shell
+    import pywintypes
+    import winerror
+    from win32com.shell import shell, shellcon
     import win32api
     import win32gui
     import win32con
@@ -32,11 +35,23 @@ except:
 
 
 def get_my_documents():
-    """Return the My Docuemnts folder"""
+    """Return the My Documents folder"""
+    # See:
+    # http://msdn.microsoft.com/en-us/library/windows/desktop/bb776887%28v=vs.85%29.aspx#mydocs
+    # http://msdn.microsoft.com/en-us/library/bb762494%28v=vs.85%29.aspx#csidl_personal
     
-    df = shell.SHGetDesktopFolder()
-    pidl = df.ParseDisplayName(0, None,  
-        "::{450d8fba-ad25-11d0-98a8-0800361b1103}")[1]
+    try:
+        df = shell.SHGetDesktopFolder()
+        pidl = df.ParseDisplayName(0, None,  
+            "::{450d8fba-ad25-11d0-98a8-0800361b1103}")[1]
+    except pywintypes.com_error as e:
+        if e.hresult == winerror.E_INVALIDARG:
+            # This error occurs when the My Documents virtual folder is not available below the Desktop virtual folder in the file system.
+            # This may be the case if it has been made unavailable using a Group Policy setting.
+            # See http://technet.microsoft.com/en-us/library/cc978354.aspx.   
+            pidl = shell.SHGetSpecialFolderLocation(0, shellcon.CSIDL_PERSONAL)
+        else:
+            raise
     mydocs = shell.SHGetPathFromIDList(pidl)
 
     # TODO: may need to handle window-specific encoding here.
