f52b5f8eec36aba0561e7b50fd6dd5387ccb40c5
Matt Rasmussen <matt.rasmus@gmail.com>
2015-03-03 12:40:41 -0800

    split out FileFS into its own module

--- keepnote/notebook/connection/fs/__init__.py
+++ keepnote/notebook/connection/fs/__init__.py
@@ -91,42 +91,37 @@ import xml.etree.cElementTree as ET
 
 
 # keepnote imports
+import keepnote
 from keepnote import safefile, plist, maskdict
 from keepnote import trans
+import keepnote.notebook
+from keepnote.notebook.connection import ConnectionError
+from keepnote.notebook.connection import NodeExists
+from keepnote.notebook.connection import NoteBookConnection
+from keepnote.notebook.connection import UnknownNode
 from keepnote.notebook.connection.fs import index as notebook_index
+from keepnote.notebook.connection.fs.file import FileFS
+from keepnote.notebook.connection.fs.file import get_node_filename
+from keepnote.notebook.connection.fs.paths import get_node_meta_file
+from keepnote.notebook.connection.fs.paths import NODE_META_FILE
 from keepnote.notebook.connection.index import AttrIndex
 
-from keepnote.notebook.connection import \
-    NoteBookConnection, UnknownNode, FileError, UnknownFile, NodeExists, \
-    ConnectionError, path_join
-# path_basename
-import keepnote
-import keepnote.notebook
 
 _ = trans.translate
 
-
 # constants
-
 XML_HEADER = u"""\
 <?xml version="1.0" encoding="UTF-8"?>
 """
 
-NODE_META_FILE = u"node.xml"
 NOTEBOOK_META_DIR = u"__NOTEBOOK__"
 LOSTDIR = u"lost_found"
 ORPHANDIR = u"orphans"
 MAX_LEN_NODE_FILENAME = 40
-NULL = object()
 
 
 #=============================================================================
 # filenaming scheme
-
-def get_node_meta_file(nodepath):
-    """Returns the metadata file for a node"""
-    return os.path.join(nodepath, NODE_META_FILE)
-
 
 def get_pref_file(nodepath):
     """Returns the filename of the notebook preference file"""
@@ -147,56 +142,6 @@ def get_orphandir(nodepath, nodeid=None):
                                 nodeid[:2])
     else:
         return os.path.join(nodepath, NOTEBOOK_META_DIR, ORPHANDIR)
-
-
-def path_local2node(filename):
-    """
-    Converts a local path to a node path
-
-    On unix:
-
-      aaa/bbb/ccc  =>  aaa/bbb/ccc
-
-    On windows:
-
-      aaa\bbb\ccc  =>  aaa/bbb/ccc
-    """
-
-    if os.path.sep == u"/":
-        return filename
-    return filename.replace(os.path.sep, u"/")
-
-
-def path_node2local(filename):
-    """
-    Converts a node path to a local path
-
-    On unix:
-
-      aaa/bbb/ccc  =>  aaa/bbb/ccc
-
-    On windows:
-
-      aaa/bbb/ccc  =>  aaa\bbb\ccc
-    """
-
-    if os.path.sep == u"/":
-        return filename
-    return filename.replace(u"/", os.path.sep)
-
-
-def get_node_filename(node_path, filename):
-    """
-    Returns a full local path to a node file
-
-    node_path  -- local path to a node
-    filename   -- node path to attached file
-    """
-
-    if filename.startswith("/"):
-        filename = filename[1:]
-
-    return os.path.join(node_path, path_node2local(filename))
 
 
 # TODO: think about how to handle "." and ".." in filenames
@@ -588,167 +533,6 @@ class PathCache (object):
             if parent and parent is not self._root_parent:
                 # update cache
                 parent.children.add(node)
-
-
-#=============================================================================
-# NotebookFile File-System API
-
-class FileFS(object):
-    """
-    Implements the NoteBook File API using the file-system.
-    """
-
-    def __init__(self, nodeid2path):
-        """
-        nodeid2path: a function that returns a filesystem path for a nodeid.
-        """
-        self._nodeid2path = nodeid2path
-
-    def get_node_path(self, nodeid):
-        return self._nodeid2path(nodeid)
-
-    def open_file(self, nodeid, filename, mode="r", codec=None, _path=None):
-        """Open a node file"""
-        if mode not in "rwa":
-            raise FileError("mode must be 'r', 'w', or 'a'")
-
-        if filename.endswith("/"):
-            raise FileError("filename '%s' cannot end with '/'" % filename)
-
-        path = self.get_node_path(nodeid) if _path is None else _path
-        fullname = get_node_filename(path, filename)
-        dirpath = os.path.dirname(fullname)
-
-        try:
-            if not os.path.exists(dirpath):
-                os.makedirs(dirpath)
-
-            # NOTE: always use binary mode to ensure no
-            # Window-specific line ending conversion
-            stream = safefile.open(fullname, mode + "b", codec=codec)
-        except Exception, e:
-            raise FileError(
-                "cannot open file '%s' '%s': %s" %
-                (nodeid, filename, str(e)), e)
-
-        return stream
-
-    def delete_file(self, nodeid, filename, _path=None):
-        """Delete a node file"""
-        path = self.get_node_path(nodeid) if _path is None else _path
-        filepath = get_node_filename(path, filename)
-
-        try:
-            if os.path.isfile(filepath):
-                os.remove(filepath)
-            elif os.path.isdir(filepath):
-                shutil.rmtree(filepath)
-            else:
-                # filename may not exist, delete is successful by default
-                pass
-        except Exception, e:
-            raise FileError("error deleting file '%s' '%s'" %
-                            (nodeid, filename), e)
-
-    def create_dir(self, nodeid, filename, _path=None):
-        """Create directory within node."""
-        if not filename.endswith("/"):
-            raise FileError("filename '%s' does not end with '/'" % filename)
-
-        path = self.get_node_path(nodeid) if _path is None else _path
-        fullname = get_node_filename(path, filename)
-
-        try:
-            if not os.path.isdir(fullname):
-                os.makedirs(fullname)
-        except Exception, e:
-            raise FileError(
-                "cannot create dir '%s' '%s'" % (nodeid, filename), e)
-
-    def list_dir(self, nodeid, filename="/", _path=None):
-        """List data files in node."""
-        if not filename.endswith("/"):
-            raise FileError("filename '%s' does not end with '/'" % filename)
-
-        path = self.get_node_path(nodeid) if _path is None else _path
-        path = get_node_filename(path, filename)
-
-        try:
-            filenames = os.listdir(path)
-        except:
-            raise UnknownFile("cannot file file '%s' '%s'" %
-                              (nodeid, filename))
-
-        for name in filenames:
-            if (name != NODE_META_FILE and
-                    not name.startswith("__")):
-                fullname = os.path.join(path, name)
-                node_fullname = path_join(filename, name)
-                if not os.path.exists(get_node_meta_file(fullname)):
-                    # ensure directory is not a node
-                    if os.path.isdir(fullname):
-                        yield node_fullname + "/"
-                    else:
-                        yield node_fullname
-
-    def has_file(self, nodeid, filename, _path=None):
-        """Return True if file exists."""
-        path = self.get_node_path(nodeid) if _path is None else _path
-        if filename.endswith("/"):
-            return os.path.isdir(get_node_filename(path, filename))
-        else:
-            return os.path.isfile(get_node_filename(path, filename))
-
-    def move_file(self, nodeid1, filename1, nodeid2, filename2,
-                  _path1=None, _path2=None):
-        """Rename a node file."""
-        path1 = self.get_node_path(nodeid1) if _path1 is None else _path1
-        path2 = self.get_node_path(nodeid2) if _path2 is None else _path2
-        filepath1 = get_node_filename(path1, filename1)
-        filepath2 = get_node_filename(path2, filename2)
-        try:
-            # remove files in the way
-            if os.path.isfile(filepath2):
-                os.remove(filepath2)
-            if os.path.isdir(filename2):
-                shutil.rmtree(filepath2)
-
-            # rename file
-            os.rename(filepath1, filepath2)
-        except Exception, e:
-            raise FileError("could not move file '%s' '%s'" %
-                            (nodeid1, filename1), e)
-
-    def copy_file(self, nodeid1, filename1, nodeid2, filename2,
-                  _path1=None, _path2=None):
-        """
-        Copy a file between two nodes.
-
-        If nodeid is None, filename is assumed to be a local file.
-        """
-        # Determine full filenames.
-        if nodeid1 is None:
-            fullname1 = filename1
-        else:
-            path1 = self.get_node_path(nodeid1) if not _path1 else _path1
-            fullname1 = get_node_filename(path1, filename1)
-
-        if nodeid2 is None:
-            fullname2 = filename2
-        else:
-            path2 = self.get_node_path(nodeid2) if not _path2 else _path2
-            fullname2 = get_node_filename(path2, filename2)
-
-        try:
-            if os.path.isfile(fullname1):
-                shutil.copy(fullname1, fullname2)
-            elif os.path.isdir(fullname1):
-                # TODO: handle case where filename1 = "/" and
-                # filename2 could be an existing directory
-                shutil.copytree(fullname1, fullname2)
-        except Exception, e:
-            raise FileError(
-                "unable to copy file '%s' '%s'" % (nodeid1, filename1), e)
 
 
 #=============================================================================
