d3ec0e2b20d362b4345d351cafde7799f625adbf
Matt Rasmussen <matt.rasmus@gmail.com>
2015-03-16 00:55:16 -0700

    include missing dir setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # setup for KeepNote
 #
 # use the following command to install KeepNote:
@@ -26,9 +26,6 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 
-
-
-
 #=============================================================================
 # constants
 
@@ -38,12 +35,15 @@ KEEPNOTE_VERSION = keepnote.PROGRAM_VERSION_TEXT
 
 #=============================================================================
 # python and distutils imports
-import os, sys, shutil, itertools
 from distutils.core import setup
+import itertools
+import os
+import sys
 
 # py2exe module (if building on windows)
 try:
     import py2exe
+    py2exe  # ignore pyflake warning.
 except ImportError:
     pass
 
@@ -60,7 +60,6 @@ def split_path(path):
         pathlist.append(tail)
     pathlist.reverse()
     return pathlist
-    
 
 
 def get_files(path, exclude=lambda f: False):
@@ -69,6 +68,7 @@ def get_files(path, exclude=lambda f: False):
 
     if isinstance(exclude, list):
         exclude_list = exclude
+
         def exclude(filename):
             for ext in exclude_list:
                 if filename.endswith(ext):
@@ -82,7 +82,7 @@ def get_files(path, exclude=lambda f: False):
                 # exclude certain files
                 continue
             elif os.path.isdir(filename):
-                # recurse directories                
+                # recurse directories
                 walk(filename)
             else:
                 # record all other files
@@ -91,7 +91,8 @@ def get_files(path, exclude=lambda f: False):
 
     return files
 
-def get_file_lookup(files, prefix_old, prefix_new, 
+
+def get_file_lookup(files, prefix_old, prefix_new,
                     exclude=lambda f: False):
     """Create a dictionary lookup of files"""
 
@@ -100,9 +101,9 @@ def get_file_lookup(files, prefix_old, prefix_new,
 
     prefix_old = split_path(prefix_old)
     prefix_new = split_path(prefix_new)
-    lookup = {}    
+    lookup = {}
 
-    for f in files:        
+    for f in files:
         path = prefix_new + split_path(f)[len(prefix_old):]
         dirpath = os.path.join(*path[:-1])
         lookup.setdefault(dirpath, []).append(f)
@@ -137,7 +138,7 @@ freedesktop_files = [
 if "py2exe" in sys.argv:
     data_files = rc_files.items() + efiles.items() + image_files.items()
     package_data = {}
-    
+
 else:
     data_files = freedesktop_files
     package_data = {'keepnote': []}
@@ -154,17 +155,17 @@ setup(
     name='keepnote',
     version=KEEPNOTE_VERSION,
     description='A cross-platform note taking application',
-    long_description = """
-        KeepNote is a cross-platform note taking application.  Its features 
+    long_description="""
+        KeepNote is a cross-platform note taking application.  Its features
         include:
-        
+
         - rich text editing
-        
+
           - bullet points
           - fonts/colors
           - hyperlinks
           - inline images
-          
+
         - hierarchical organization for notes
         - full text search
         - integrated screenshot
@@ -175,52 +176,55 @@ setup(
     author='Matt Rasmussen',
     author_email='rasmus@alum.mit.edu',
     url='http://keepnote.org',
-    download_url='http://keepnote.org/download/keepnote-%s.tar.gz' % KEEPNOTE_VERSION,
-    
+    download_url='http://keepnote.org/download/keepnote-%s.tar.gz' % KEEPNOTE_VERSION,  # nopep8
+
     classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Environment :: Win32 (MS Windows)',
-          'Environment :: X11 Applications',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Education',
-          'Intended Audience :: End Users/Desktop',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          ],
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
     license="GPL",
-    
-    packages=['keepnote',
-              'keepnote.gui',
-              'keepnote.gui.richtext',
-              'keepnote.notebook',
-              'keepnote.notebook.connection',
-              'keepnote.notebook.connection.fs',
-              'keepnote.compat',
-              'keepnote.mswin'],
+
+    packages=[
+        'keepnote',
+        'keepnote.compat',
+        'keepnote.gui',
+        'keepnote.gui.richtext',
+        'keepnote.notebook',
+        'keepnote.notebook.connection',
+        'keepnote.notebook.connection.fs',
+        'keepnote.server',
+        'keepnote.mswin'
+    ],
     scripts=['bin/keepnote'],
     data_files=data_files,
     package_data=package_data,
-    
+
     windows=[{
         'script': 'bin/keepnote',
         'icon_resources': [(1, 'keepnote/images/keepnote.ico')],
         }],
-    options = {
-        'py2exe' : {
+    options={
+        'py2exe': {
             'packages': 'encodings',
-            'includes': 'cairo,pango,pangocairo,atk,gobject,win32com.shell,win32api,win32com,win32ui,win32gui',
+            'includes': 'cairo,pango,pangocairo,atk,gobject,win32com.shell,win32api,win32com,win32ui,win32gui',  # nopep8
             'dist_dir': 'dist/keepnote-%s.win' % KEEPNOTE_VERSION
         },
         #'sdist': {
         #    'formats': 'zip',
         #}
     }
-    )
+)
 
 
 # execute post-build script
